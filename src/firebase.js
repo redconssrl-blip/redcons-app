518d28fd668fca647ec3a1f668325a8a2ce79f7f
redconssrl-blip <redconssrl@gmail.com>
2026-07-06 23:13:08 -0300

    "Credenciales Firebase"

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// ⚠️ REEMPLAZÁ estos valores con los de TU proyecto Firebase
-// (los obtenés en el Paso 3 de las instrucciones)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROJECT.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDHD94Bt_KiJz-rxjhlwWadkNQnn_th4sc",
+  authDomain: "redcons-app.firebaseapp.com",
+  projectId: "redcons-app",
+  storageBucket: "redcons-app.firebasestorage.app",
+  messagingSenderId: "297829889400",
+  appId: "1:297829889400:web:ebc367f0e253af429a048d"
 }
 
 const app = initializeApp(firebaseConfig)
